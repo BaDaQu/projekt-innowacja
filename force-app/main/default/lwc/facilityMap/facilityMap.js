bffeb23268d8b92ad56a971b9bec6e05a7865179
filip <251186@edu.p.lodz.pl>
2025-01-23 15:46:55 +0100

    mapa v3

--- force-app/main/default/lwc/facilityMap/facilityMap.js
+++ force-app/main/default/lwc/facilityMap/facilityMap.js
@@ -1,6 +1,5 @@
 import { LightningElement, api, wire } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
-import facilityname from '@salesforce/schema/Medical_Facility_Name__c';
 const FIELDS = ['Medical_Facility__c.Medical_Facility_Name__c', 'Medical_Facility__c.Address__c'];
 
 export default class FacilityMap extends LightningElement {
@@ -13,17 +12,29 @@ export default class FacilityMap extends LightningElement {
     @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
     loadFacilityLocation({ error, data }) {
         if (data) {
+            const address = data.fields.Address__c.value;
+            const facilityName = data.fields.Medical_Facility_Name__c.value;
             this.mapMarkers = [
                 {
                     location: {
-                        Street: data.fields.Street,
-                        City: data.fields.City,
-                        Country: data.fields.Country,
-                },
-                    title: facilityname,
+                        Street: address.street,
+                        City: address.city,
+                        State: address.state,
+                        PostalCode: address.postalCode,
+                        Country: address.country,
+                    },
+                    title: facilityName,
                 }
             ];
-            this.center = { location: { Street: data.fields.Street, City: data.fields.City, Country: data.fields.Country }, };
+            this.center = {
+                    location: {
+                        Street: address.street,
+                        City: address.city,
+                        State: address.state,
+                        PostalCode: address.postalCode,
+                        Country: address.country,
+                }
+            };
             this.error = undefined;
         } else if (error) {
             this.error = error;
