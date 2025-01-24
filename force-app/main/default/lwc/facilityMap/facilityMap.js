1be3ce48e3b7cad478152d4803f159b3da570766
filip <149077794+jxjdffvbbsdgkasd@users.noreply.github.com>
2025-01-24 17:07:03 +0100

    Revert "[PI-020] mapa"

--- force-app/main/default/lwc/facilityMap/facilityMap.js
+++ force-app/main/default/lwc/facilityMap/facilityMap.js
@@ -1,52 +1,42 @@
 import { LightningElement, api, wire } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
-const FIELD = ['Medical_Facility__c.Medical_Facility_Name__c'];
-import getStreets from '@salesforce/apex/FacilityMapHelper.getStreets';
-import getCitys from '@salesforce/apex/FacilityMapHelper.getCitys';
-import getCountrys from '@salesforce/apex/FacilityMapHelper.getCountrys';
-import getPostalCodes from '@salesforce/apex/FacilityMapHelper.getPostalCodes';
-
-
+const FIELDS = ['Medical_Facility__c.Medical_Facility_Name__c', 'Medical_Facility__c.Address__c'];
 
 export default class FacilityMap extends LightningElement {
-    @api recordId;
+    @api facilityId;
 
     mapMarkers = [];
     center = {};
     error;
 
-    @wire(getRecord, { recordId: '$recordId', fields: FIELD })
-    async loadFacilityLocation({ error, data }) {
+    @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
+    loadFacilityLocation({ error, data }) {
         if (data) {
-            const facilityName = data.fields.Medical_Facility_Name__c;
-            const street =await getStreets({name: data.fields.Medical_Facility_Name__c.value});
-            const city =await  getCitys({name: data.fields.Medical_Facility_Name__c.value});
-            const code =await  getPostalCodes({name: data.fields.Medical_Facility_Name__c.value});
-            const country =await  getCountrys({name: data.fields.Medical_Facility_Name__c.value});
-            console.log('street: ',street,',city: ',city,',code: ',code,',country: ',country,',facility name: ',facilityName);
+            console.log('dzialam !!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+            console.log('dzialam !!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+            const address = data.fields.Address__c.value;
+            const facilityName = data.fields.Medical_Facility_Name__c.value;
             this.mapMarkers = [
                 {
                     location: {
-                        City: String(city),
-                        Country: String(country),
-                        PostalCode: String(code),
-                        Street: String(street),
+                        Street: address.street,
+                        City: address.city,
+                        State: address.state,
+                        PostalCode: address.postalCode,
+                        Country: address.country,
                     },
-                    value: 'location001',
-                    title: facilityName.value,
+                    title: facilityName,
                 }
             ];
-            console.log('Full Map Markers:', JSON.stringify(this.mapMarkers, null, 2));
             this.center = {
                     location: {
-                        City: String(city),
-                        Country: String(country),
-                        PostalCode: String(code),
-                        Street: String(street),
+                        Street: address.street,
+                        City: address.city,
+                        State: address.state,
+                        PostalCode: address.postalCode,
+                        Country: address.country,
                 }
             };
-            console.log('Full Center:', JSON.stringify(this.center, null, 2));
-
             this.error = undefined;
         } else if (error) {
             console.log('!!!!!!!nie dzialam!!!!!!!!!!!!!!!', error);
