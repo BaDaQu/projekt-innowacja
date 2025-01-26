116aa466e721109f02f702e53d18126f65064c11
BaDaQu <bartek.raj2@gmail.com>
2025-01-26 16:59:19 +0100

    [PI-019-obnap1]

--- force-app/main/default/lwc/facilityHoursComponent/facilityHoursComponent.js
+++ force-app/main/default/lwc/facilityHoursComponent/facilityHoursComponent.js
@@ -7,9 +7,7 @@ import { NavigationMixin } from 'lightning/navigation';
 const COLUMNS = [
     { label: 'Day of Week', fieldName: 'Day_of_Week__c', type: 'text' },
     { label: 'Opening Time', fieldName: 'Opening_Time__c', type: 'text' },
-    { label: 'Closing Time', fieldName: 'Closing_Time__c', type: 'text' },
-    { label: 'Is Open', fieldName: 'Is_Open__c', type: 'boolean' },
-    { label: 'Additional Information', fieldName: 'Additional_Information__c', type: 'text' }
+    { label: 'Closing Time', fieldName: 'Closing_Time__c', type: 'text' }
 ];
 
 export default class FacilityHoursComponent extends NavigationMixin(LightningElement) {
@@ -29,22 +27,21 @@ export default class FacilityHoursComponent extends NavigationMixin(LightningEle
             'Facility_Hours__c.Id',
             'Facility_Hours__c.Day_of_Week__c',
             'Facility_Hours__c.Opening_Time__c',
-            'Facility_Hours__c.Closing_Time__c',
-            'Facility_Hours__c.Is_Open__c',
-            'Facility_Hours__c.Additional_Information__c'
+            'Facility_Hours__c.Closing_Time__c'
             ]
       })
       wiredFacilityHours({ data, error }) {
           if (data) {
             this.facilityHoursData = data.records.map(record => {
-                return {
-                    Id: record.fields.Id.value,
-                    Day_of_Week__c: record.fields.Day_of_Week__c.value,
-                    Opening_Time__c: record.fields.Opening_Time__c.value,
-                    Closing_Time__c: record.fields.Closing_Time__c.value,
-                    Is_Open__c: record.fields.Is_Open__c.value,
-                    Additional_Information__c: record.fields.Additional_Information__c.value
-                }
+              const openingTime = record.fields.Opening_Time__c.value;
+              const closingTime = record.fields.Closing_Time__c.value;
+
+              return {
+                  Id: record.fields.Id.value,
+                  Day_of_Week__c: record.fields.Day_of_Week__c.value,
+                  formattedOpeningTime: this.formatTime(openingTime),
+                  formattedClosingTime: this.formatTime(closingTime)
+              }
              });
           } else if (error) {
               console.error('Error loading facility hours:', error);
@@ -52,4 +49,10 @@ export default class FacilityHoursComponent extends NavigationMixin(LightningEle
           }
       }
 
+
+      formatTime(timeString) {
+          if (!timeString) return '';
+           const [hours, minutes] = timeString.split(':');
+          return `${hours}:${minutes}`;
+    }
 }
