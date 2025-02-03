1bd69645c6ac680264af04408028463543cde61f
filip <251186@edu.p.lodz.pl>
2025-02-03 17:25:39 +0100

    parse obiektu, jedna motoda

--- force-app/main/default/lwc/facilityMap/facilityMap.js
+++ force-app/main/default/lwc/facilityMap/facilityMap.js
@@ -1,16 +1,13 @@
 import { LightningElement, api, wire } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
 const FIELD = ['Medical_Facility__c.Medical_Facility_Name__c'];
-import getStreets from '@salesforce/apex/FacilityMapHelper.getStreets';
-import getCitys from '@salesforce/apex/FacilityMapHelper.getCitys';
-import getCountrys from '@salesforce/apex/FacilityMapHelper.getCountrys';
-import getPostalCodes from '@salesforce/apex/FacilityMapHelper.getPostalCodes';
+import getAddress from '@salesforce/apex/FacilityMapHelper.getAddress';
 
 
 
 export default class FacilityMap extends LightningElement {
     @api recordId;
-
+    Address;
     mapMarkers = [];
     center = {};
     error;
@@ -19,34 +16,29 @@ export default class FacilityMap extends LightningElement {
     async loadFacilityLocation({ error, data }) {
         if (data) {
             const facilityName = data.fields.Medical_Facility_Name__c;
-            const street =await getStreets({name: data.fields.Medical_Facility_Name__c.value});
-            const city =await  getCitys({name: data.fields.Medical_Facility_Name__c.value});
-            const code =await  getPostalCodes({name: data.fields.Medical_Facility_Name__c.value});
-            const country =await  getCountrys({name: data.fields.Medical_Facility_Name__c.value});
-            console.log('street: ',street,',city: ',city,',code: ',code,',country: ',country,',facility name: ',facilityName);
+            const res =await getAddress({name: data.fields.Medical_Facility_Name__c.value});
+            this.Address = JSON.parse(res);
+            console.log('adres:',res);
             this.mapMarkers = [
                 {
                     location: {
-                        City: String(city),
-                        Country: String(country),
-                        PostalCode: String(code),
-                        Street: String(street),
+                        City: this.Address.city,
+                        Country: this.Address.country,
+                        PostalCode: this.Address.postalCode,
+                        Street: this.Address.street,
                     },
                     value: 'location001',
                     title: facilityName.value,
                 }
             ];
-            console.log('Full Map Markers:', JSON.stringify(this.mapMarkers, null, 2));
             this.center = {
                     location: {
-                        City: String(city),
-                        Country: String(country),
-                        PostalCode: String(code),
-                        Street: String(street),
+                        City: this.Address.city,
+                        Country: this.Address.country,
+                        PostalCode: this.Address.postalCode,
+                        Street: this.Address.street,
                 }
             };
-            console.log('Full Center:', JSON.stringify(this.center, null, 2));
-
             this.error = undefined;
         } else if (error) {
             console.log('!!!!!!!nie dzialam!!!!!!!!!!!!!!!', error);
